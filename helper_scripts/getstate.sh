fdd554fa59e1a556da594b19784759c6b3559018
Robin Norwood <rnorwood@hashicorp.com>
2025-01-08 15:45:38 -0600

    Update config and fix bug in script

--- helper_scripts/getstate.sh
+++ helper_scripts/getstate.sh
@@ -2,10 +2,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-
 HTTP_RESPONSE=$(curl \
      --header "Authorization: Bearer "$TFC_TOKEN"" \
      --header "Content-Type: application/vnd.api+json" \
      "https://app.terraform.io/api/v2/workspaces/"$WORKSPACE_ID"/current-state-version" | jq -r '.data | .attributes | ."hosted-state-download-url"')
 
-curl -o state.tfstate $HTTP_RESPONSE
+curl \
+   -o state.tfstate \
+   -L \
+   --header "Authorization: Bearer $TFC_TOKEN" \
+   $HTTP_RESPONSE
